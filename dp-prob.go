6482b3bb837cf9bfd8b222ff19bc5697a1500e44
Rajan Ponnappan <rajan.ponnappan@getcruise.com>
2017-05-11 23:29:00 -0700

    final working soln with step

--- dp-prob.go
+++ dp-prob.go
@@ -1,37 +1,49 @@
 package main
 import ("fmt")
 
-var m = map[int] int {} 
+type ans struct { 
+	step int
+	count int
+} 
+
+var m = map[int] ans {} 
 func solve (n int) int { 
 	if n == 1 { 
 		return 0
 	} 
 	
-	if m[n] != 0 { 
-		return m[n]
+	if m[n].count != 0 { 
+		return m[n].count
 	}
 	if n % 3 == 0 { 
-		m[n] = solve(n/3) + 1 
+		a :=  ans{ solve(n/3) + 1, n/3} 
+		m[n] =a 
 	} 
 	var s int
 	if n % 2 == 0 { 
 		s = solve(n/2) + 1 
-		if m[n] == 0 || m[n] > s {
-			m[n] = s
+		if m[n].count == 0 || m[n].count > s {
+			a :=  ans{ s, n/2} 
+			m[n] =a 
 		}	
 	} 
 	s = solve(n-1) + 1 
-	if m[n] ==0 || m[n] > s { 
-		m[n] = s
+	if m[n].count  ==0 || m[n].count > s { 
+		a :=  ans{ s, n - 1} 
+		m[n] =a 
 	}
-	return m[n]
+	return m[n].count
 } 
 
 
 func main() {
-	var m int
-	fmt.Scanf("%d", &m)
-	fmt.Println(solve(m))
+	var m1 int
+	fmt.Scanf("%d", &m1)
+	solve(m1) 
+	for m1 >= 1 { 
+		fmt.Println(m1)
+		m1 = m[m1].step  
+	} 
 }
 
 
