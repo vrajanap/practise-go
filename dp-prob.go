925bb1c7b055473c6c88fe20f3e32fe79c553a61
Rajan Ponnappan <rajan.ponnappan@getcruise.com>
2017-05-11 22:45:26 -0700

    more substance

--- dp-prob.go
+++ dp-prob.go
@@ -3,10 +3,31 @@ import ("fmt" )
 
 var m = map[int] int {} 
 func solve (n int) int { 
-	m[n] = 1	
-	return 0
+	if m[n] != 0 { 
+		return m[n]
+	}
+	if n % 3 == 0 { 
+		m[n] = solve(n/3) + 1 
+	} 
+	var s int
+	if n % 2 == 0 { 
+		s = solve(n/2) + 1 
+		if m[n] == 0 || m[n] > s {
+			m[n] = s
+		}	
+	} 
+	s = solve(n-1) + 1 
+	if m[n] ==0 || m[n] > s { 
+		m[n] = s
+	}
+	return m[n]
 } 
 
 
 func main() {
+	
+	fmt.Println("%v\n", solve(9))
+	
 }
+
+
